765cc284cbe337dbc751937419d7ebdb260c6d3b
Karol de Castro <karolinycosta@yahoo.com.br>
2021-05-12 20:14:55 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+
+  public static void  main(String[] args) {
+    Carros c1 = new Carros();
+    c1.potencia = 10;
+    c1.nome = "Corcel";
+    c1.velocidade = 0;
+
+    Carros c2 = new Carros();
+    c2.potencia = 15;
+    c2.nome = "Carango";
+    c2.velocidade = 0;
+
+    c1.acelerar();
+    c1.frear();
+
+    c1.imprimir();
+    c2.imprimir();
+
+  }
+}
