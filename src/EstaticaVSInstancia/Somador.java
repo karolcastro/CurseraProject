7d900d049a8cdcba0809aa546611e51f22bc6b02
Karol de Castro <karolinycosta@yahoo.com.br>
2021-05-19 20:38:22 -0300

    adding EstaticaVSInstancia

---
+++ src/EstaticaVSInstancia/Somador.java
@@ -0,0 +1,20 @@
+package EstaticaVSInstancia;
+
+public class Somador {
+
+    String nome;
+
+    int valorInstancia = 0;
+
+    static int valorEstatica = 0;
+
+    void somar(){
+        valorInstancia++;
+        valorEstatica++;
+    }
+
+    void imprimir(){
+        System.out.println(" O Somador" +nome+": instancia="+valorInstancia+ " e estatica=" +valorEstatica);
+
+    }
+}
